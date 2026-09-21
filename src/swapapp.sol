4db14bf6c0829148658219f9ecaa2e6fb3c4b6b3
david123456858 <juandavidperaltafuentes@gmail.com>
2026-09-21 11:01:24 -0500

    Agregar importación de IERC20 y transferir tokens en la función swapToken

--- src/swapapp.sol
+++ src/swapapp.sol
@@ -6,6 +6,7 @@
 pragma solidity 0.8.28;
 
 import {IV2Router02} from "../src/interface/I2Router02.sol";
+import { IERC20 } from "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract swapapp {
     address public V2Router02;
@@ -18,10 +19,12 @@ contract swapapp {
     function swapToken(
         uint256 amountIn_,
         uint256 amountOutMin_,
-        address[] calldata path_,
+        address[] calldata path_, 
         uint256 deadline_
     ) external {
         
+        IERC20(path_[0]).transferFrom(msg.sender, address(this), amountIn_);
+
         uint256[] memory amountsOut = IV2Router02(V2Router02).swapExactTokensForTokens(amountIn_, amountOutMin_, path_, msg.sender, deadline_);
 
         emit SwapToken(path_[0], path_[path_.length - 1], amountIn_, amountsOut[amountsOut.length - 1]);
