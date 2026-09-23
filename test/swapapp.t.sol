0398ead62ec85f654b4c76c256e93c0d81e234f3
david123456858 <juandavidperaltafuentes@gmail.com>
2026-09-22 20:41:37 -0500

    Agregar pruebas para la función testSwapToken y definir la dirección del usuario en swappTest

--- test/swapapp.t.sol
+++ test/swapapp.t.sol
@@ -13,11 +13,19 @@ contract swappTest is Test {
 
     Swapapp app;
     address uniswapV2SwapRouter = 0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24;
+    address user = 0xd9EabE84F41F15eDD4a17E94de8Ea81291dd365a;
+
     function setUp() public{
         app = new Swapapp(uniswapV2SwapRouter);
     }
 
     function testHasBeenDeploymentCorrectly () public view {
         assert(app.V2Router02() == uniswapV2SwapRouter);
+    }
+
+    function testSwapTokenCorrectly() public {
+        vm.startPrank(user);
+
+        vm.stopPrank();
     }
 }
